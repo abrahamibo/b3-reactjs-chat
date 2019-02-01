8cf2d5eda773913c879df33998b1c39699ba64a1
abraham <abraham.ibo@hetic.net>
2019-02-01 17:50:34 +0100

    build

---
+++ build/precache-manifest.03124ff68751078b3b75d28a1ea91862.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5035df7196bb5dc8ca6d",
+    "url": "/static/js/main.5035df71.chunk.js"
+  },
+  {
+    "revision": "b287c602cfc6e7bd2184",
+    "url": "/static/js/1.b287c602.chunk.js"
+  },
+  {
+    "revision": "5035df7196bb5dc8ca6d",
+    "url": "/static/css/main.04ce950e.chunk.css"
+  },
+  {
+    "revision": "62bf6a3a9a8d3aa74ce7a4d6683c3d85",
+    "url": "/index.html"
+  }
+];
